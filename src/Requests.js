1bb9dccea3d884c63bea963d126f28c111902d46
chinmayee gowda <gowdachinmayee3@gmail.com>
2024-06-06 08:52:03 +0530

    Initial commit

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '2f3800de1aa5748716c179d627f7d065'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
